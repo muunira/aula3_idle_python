5f6e25a963088bf4b95b707216df6fb021baea16
Munira Majdoub <munira_majdoub@hotmail.com>
2024-10-17 18:00:56 -0300

    Add files via upload

---
+++ ATIVIDADE_AULA3.py
@@ -0,0 +1,20 @@
+# dado do tipo interger (inteiro)
+numero = 10
+print(numero)
+
+# dado do tipo string (texo)
+texto = 'isso é um texto'
+print(texto)
+
+# dado do tipo float (real)
+real = 10.5
+print(real)
+
+# dado do tipo bool(lógico)
+boleano = True
+print(boleano)
+
+num1 = 3450
+num2 = 44958
+resultado = num1+num2
+print(resultado)
